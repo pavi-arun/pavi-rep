b0219733326d102cf2b728900c634e8680fb825a
pavi <dhanamarun97@gmail.com>
2024-06-20 17:00:49 +0530

    The code was added

---
+++ src/org/javaclass/FirstClass.java
@@ -0,0 +1,19 @@
+package org.javaclass;
+
+public class FirstClass {
+	public void day1(){
+		System.out.println("welcome to day1");
+	}
+	public void oops(){
+		System.out.println("Tdy class was oops topic");
+	}
+
+	public static void main(String[] args) {
+		
+		FirstClass fC=new FirstClass();
+		fC.day1();
+		fC.oops();
+
+	}
+
+}
